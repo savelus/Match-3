5172ff97272ef3cf206ea97b6196025730520a0f
savelus <savelussava@mail.ru>
2022-11-17 00:21:58 +0500

    Add View Sttars in levelselectmenu

--- Assets/Scripts/LevelSelectButton.cs
+++ Assets/Scripts/LevelSelectButton.cs
@@ -10,7 +10,24 @@ public class LevelSelectButton : MonoBehaviour
     public GameObject star1, star2, star3;
     void Start()
     {
-        
+        star1.SetActive(false);
+        star2.SetActive(false);
+        star3.SetActive(false);
+
+        if(PlayerPrefs.HasKey(levelToLoad + "_Star1"))
+        {
+            star1.SetActive(true);
+        }
+
+        if (PlayerPrefs.HasKey(levelToLoad + "_Star2"))
+        {
+            star2.SetActive(true);
+        }
+
+        if (PlayerPrefs.HasKey(levelToLoad + "_Star3"))
+        {
+            star3.SetActive(true);
+        }
     }
 
     public void LoadLevel()
