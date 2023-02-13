57d53e7d158fb6cb49317956975d2c2832e17e4a
savelus <savelussava@mail.ru>
2023-02-13 11:48:32 +0500

    Add Move Tile to Monster

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,8 +1,11 @@
+using DG.Tweening;
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
 using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour
@@ -188,14 +191,31 @@ public class Board : MonoBehaviour
                 {
                     SFXManager.instance.PlayGemBreak();
                 }
+
                 Instantiate(allGems[position.x, position.y].destroyEffect, 
                     new Vector2(position.x, position.y), Quaternion.identity);
 
-                Destroy(allGems[position.x, position.y].gameObject);
-                allGems[position.x, position.y] = null;
+                MoveAndDestroyGemOnMonster(position);
+
             }
         }
     }
+
+    private void MoveAndDestroyGemOnMonster(Vector2Int position)
+    {
+
+        var monsterPosition = monsterManager.Monster.transform.position;
+        var gem = allGems[position.x, position.y];
+        allGems[position.x, position.y] = null;
+        DOTween.Sequence().Append(gem.transform.DOMove(monsterPosition, 0.3f))
+            .Join(gem.transform.DOScale(0.5f, 0.3f))
+            .AppendInterval(0.1f)
+            .AppendCallback(() => {
+                Debug.Log("fffff");
+                Destroy(gem.gameObject);
+            });
+    }
+
 
     public void DestroyMatches()
     {
@@ -344,7 +364,6 @@ public class Board : MonoBehaviour
             
         }
 
-        
     }
 
     public void scoreCheck(Gem gemToCheck)
